ed60238c9305c8e1bf40d77aa0157d3e1c0bf6ee
gheezl <gheezl25@gmail.com>
2020-08-05 18:23:08 -0400

    Filling out the home page more

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,9 +1,16 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 import "./homepage.css"
 
 
 const HomePage = () => {
+    let [toggleIntroduction, setToggleIntroduction] = useState(false)
+
+
+    const onClickFunction = () => {
+        setToggleIntroduction(!toggleIntroduction)
+    }
+
     return (
         <Fragment>
             <div>
@@ -13,14 +20,23 @@ const HomePage = () => {
                     <img alt="pokeball" className="pokemon-image" src="pokemon.png" height="50px" />
                 </div>
                 <div className="introduction">
-                    <span className="introduction-message">Introduction</span>
-                    <div className="introduction-content">
-                        <span className="introduction-paragraph">
-                            Hi! Welcome to your Pokédex, here you will find access to more than 900 pokemon and all of their relevant information. You can view all Pokémon by clicking on the View all tab
-                            on your top right, or you can use the Search bar to look up a specific Pokémon. Have fun!
-                        </span>
-                        <img alt="" className="intoduction-image" src="https://wallpapercave.com/wp/uk90alx.jpg" height="249px" width="398px" />
-                    </div>
+                    <span onClick={onClickFunction} className="introduction-message">Introduction ❱</span>
+                    {
+                        toggleIntroduction
+                            ? (<div className="introduction-content">
+                                <span className="introduction-paragraph">
+                                    Hi! Welcome to your Pokédex, here you will find access to more than 900 pokemon and all of their relevant information. You can view all Pokémon by clicking on the View all tab
+                                    on your top right, or you can use the Search bar to look up a specific Pokémon. Have fun!
+                                </span>
+                                <img alt="" className="intoduction-image" src="https://wallpapercave.com/wp/uk90alx.jpg" height="249px" width="398px" />
+                            </div>
+
+                            )
+                            : (
+                                null
+                            )
+
+                    }
                 </div>
             </div>
         </Fragment>
